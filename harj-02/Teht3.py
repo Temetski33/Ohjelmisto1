f6c6c29cbb11ca348546fc2adbc26a88ce24a950
Temetski33 <teemu.laasio@metropolia.fi>
2024-08-21 14:40:26 +0300

    Harjoitus 2 v1

---
+++ harj-02/Teht3.py
@@ -0,0 +1,8 @@
+kanta = input("Syötä suorakulmion kanta:")
+korkeus = input("Syötä suorakulmion korkeus:")
+kanta = float(kanta)
+korkeus = float(korkeus)
+piiri = 2 * kanta + 2 * korkeus
+ala = kanta * korkeus
+print(f'Suorakulmion ala on {ala}.')
+print(f'Suorakulmion piiri on {piiri}.')
