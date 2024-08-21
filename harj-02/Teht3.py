a35c84e0ddc95deedff23583e3091ecddc85da9a
Temetski33 <teemu.laasio@metropolia.fi>
2024-08-21 15:07:38 +0300

    Harjoitus 2 v1

--- harj-02/Teht3.py
+++ harj-02/Teht3.py
@@ -1,8 +1,11 @@
 kanta = input("Syötä suorakulmion kanta:")
 korkeus = input("Syötä suorakulmion korkeus:")
+
 kanta = float(kanta)
 korkeus = float(korkeus)
+
 piiri = 2 * kanta + 2 * korkeus
 ala = kanta * korkeus
+
 print(f'Suorakulmion ala on {ala}.')
 print(f'Suorakulmion piiri on {piiri}.')
---
+++ harj-02/Teht4.py
@@ -0,0 +1,31 @@
+while True:
+    try:
+        yks = int(input("Anna kolme kokonaislukua. Luku yksi:"))
+    except ValueError:
+        print("Ole ystävällinen ja syötä kokonaisluku.")
+        continue
+    break
+
+while True:
+    try:
+        kaks = int(input("Luku kaksi:"))
+    except ValueError:
+        print("Ole ystävällinen ja syötä kokonaisluku.")
+        continue
+    break
+
+while True:
+    try:
+        kolme = int(input("Luku kolme:"))
+    except ValueError:
+        print("Ole ystävällinen ja syötä kokonaisluku.")
+        continue
+    break
+
+summa = yks + kaks + kolme
+tulo = yks * kaks * kolme
+ka = summa / 3
+
+print(f'Lukujen summa on {summa}.')
+print(f'Lukujen tulo on {tulo}.')
+print(f'Lukujen keskiarvo on {ka}.')
