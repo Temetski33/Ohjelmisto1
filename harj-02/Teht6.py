fefd540e080d2e8dea9ed1f2005d02d371170790
Temetski33 <teemu.laasio@metropolia.fi>
2024-08-21 15:35:42 +0300

    Harjoitus 2 v1

---
+++ harj-02/Teht6.py
@@ -0,0 +1,13 @@
+import random
+
+yks = (random.randint(0, 9))
+kaks = (random.randint(0, 9))
+kolme = (random.randint(0, 9))
+
+TokaYks = (random.randint(1, 6))
+TokaKaks = (random.randint(1, 6))
+TokaKol = (random.randint(1, 6))
+TokaNel = (random.randint(1, 6))
+
+print(f'Kolmenumeroinen koodi on {yks}{kaks}{kolme}.')
+print(f'Nelinumeroinen koodi on {TokaYks}{TokaKaks}{TokaKol}{TokaNel}.')
