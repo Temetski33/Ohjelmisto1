3bed89db25479e9c2764830dc7bb0590d55c76a8
Temetski33 <teemu.laasio@metropolia.fi>
2024-09-01 18:43:23 +0300

    Moduuli 4 v1

--- harj-04/Teht5.py
+++ harj-04/Teht5.py
@@ -1,4 +1,16 @@
-tunnus = "python"
-salis = "rules"
+tunnusOikea = "python"
+salisOikea = "rules"
+counter = 0
 
-print(f"{tunnus}, {salis}")
+tunnus = "placeholder tunnus"
+salis = "placeholder salis"
+
+while (tunnus != tunnusOikea or salis != salisOikea) and counter < 5:
+    counter = counter + 1
+    tunnus = input(f"Kerro tunnus: ")
+    salis = input(f"Kerro salis: ")
+
+if counter < 5:
+    print(f"Tervetuloa")
+else:
+    print(f"Pääsy evätty")
