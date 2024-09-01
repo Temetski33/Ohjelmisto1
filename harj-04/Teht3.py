3b8d167c65ce6a3c34a68ca13f7cdd28bf2cca51
Temetski33 <teemu.laasio@metropolia.fi>
2024-09-01 19:15:27 +0300

    Moduuli 4 v1

--- harj-04/Teht3.py
+++ harj-04/Teht3.py
@@ -1,5 +1,11 @@
-luku = 0
-while luku != "":
-    luku = input(f"Syötä luku:")
-    numero = int(luku)
-    print(f"{numero}")
+luku = "placeholder luku"
+lista = []
+
+while luku != None:
+    luku = int(input(f"Syötä luku: ") or 0)
+    luku = luku if luku != 0 else None
+    lista.append(luku)
+
+lista.remove(None)
+lista.sort()
+print(f"Pienin syötetty luku on {lista[0]}, suurin syötetty luku on {lista[-1]}")
