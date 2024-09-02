752f538178f267f39e20fc0c168b8322da663ca5
Temetski33 <teemu.laasio@metropolia.fi>
2024-09-02 15:13:11 +0300

    Moduuli 5 v1

--- harj-05/Teht2.py
+++ harj-05/Teht2.py
@@ -1,5 +1,11 @@
-lista = [2, 3, 6, 47, 4]
+luku = "placeholder luku"
+lista = []
 
+while luku != None:
+    luku = int(input(f"Syötä luku (lopeta syöttämällä tyhjä): ") or 0)
+    luku = luku if luku != 0 else None
+    lista.append(luku)
+
+lista.remove(None)
 lista.sort(reverse=True)
-
-print(lista)
+print(f"Viisi suurinta lukua (laskevassa järjestyksessä): {lista[0:5]}")
