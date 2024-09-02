d90935a12dfec824d3c2c210552838472593fb77
Temetski33 <teemu.laasio@metropolia.fi>
2024-09-02 13:47:12 +0300

    Moduuli 5 tuntiesimerkkejä

---
+++ esimerkkei/Esim3.py
@@ -0,0 +1,28 @@
+"""
+Erinäistä listasäätöä
+"""
+
+nimet = []
+lkm = int(input("kuinka monta osallistujaa? "))
+
+#toistetaan nimien kysyminen tarvittava määrä
+for i in range(lkm):
+    nimi = input("Anna nimi: ")
+    #lisätään saatu nimi listan loppuun (append)
+    nimet.append(nimi)
+
+#Järjestetään nimet aakkosjärjestykseen.
+nimet.sort()
+print(f"Nimet aakkosjärjestyksessä:")
+print(nimet)
+
+#muokataan listan alkioiden sisältöjä
+nimet[1] = "Toka"
+nimet[-1] = "Vika"
+nimet.insert(0, "Uusi eka")
+
+print(nimet)
+
+#tulostetaan listan alkiot kukin omalla rivillään (for... in)
+for alkio in nimet:
+    print(alkio)
---
+++ esimerkkei/Esim4.py
@@ -0,0 +1,9 @@
+"""
+range-funktio ja for-toisto
+"""
+
+print(f"Tulostetaa parilliset luvut väliltä 10 ... 20")
+
+
+for nro in range(10, 21, 2):
+    print(nro)
