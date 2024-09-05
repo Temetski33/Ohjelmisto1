cd7f7a9020134c68436186c64e3b9982bb135ba0
Temetski33 <teemu.laasio@metropolia.fi>
2024-09-05 13:01:42 +0300

    Moduuli 6 v2

--- harj-06/Teht5.py
+++ harj-06/Teht5.py
@@ -1,6 +1,8 @@
 def paritonkiller(karsittavat):
-    karsitut = [2, 3]
-    return(karsitut)
+    for i in karsittavat:
+        if (i % 2 == 1):
+            karsittavat.remove(i)
+    return(karsittavat)
 
 lista = []
 n = int(input(f"Anna listaan syötettävien lukujen määrä : "))
@@ -9,3 +11,4 @@ for i in range(0, n):
     lista.append(numero)
 
 print(lista)
+print(paritonkiller(lista))
