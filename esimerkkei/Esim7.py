7b2b3dad20400a5b0ba427bfb926e01f80c10edc
Temetski33 <teemu.laasio@metropolia.fi>
2024-09-09 14:00:48 +0300

    Moduuli 6 v2

---
+++ esimerkkei/Esim7.py
@@ -0,0 +1,21 @@
+#Moduuli 6 teht 5 fixailua jälkeenpäin
+
+def paritonkiller(alkup):
+    karsittavat = alkup
+    print(alkup)
+    print(karsittavat)
+    for i in karsittavat:
+        if i % 2 == 1:
+            karsittavat.remove(i)
+    return(karsittavat)
+
+lista = []
+n = int(input(f"Anna listaan syötettävien lukujen määrä : "))
+for i in range(0, n):
+    numero = int(input(f"Syötä kokonaisluku listaan: "))
+    lista.append(numero)
+
+paritonkiller(lista)
+
+print(lista)
+print(paritonkiller(lista))
