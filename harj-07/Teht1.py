60504b16d26c6fce42bd816f44e322593dc9ff02
Temetski33 <teemu.laasio@metropolia.fi>
2024-09-09 14:55:16 +0300

    Moduuli 7 ekat

--- harj-07/Teht1.py
+++ harj-07/Teht1.py
@@ -1,3 +1,15 @@
+kuukausinro = int(input(f"Anna kuukauden numero: "))
+
 vuodenajat = ("kevät", "kesä", "syksy", "talvi")
 
-print(vuodenajat)
+if kuukausinro == 12 or kuukausinro == 1 or kuukausinro == 2:
+    print(vuodenajat[3])
+
+elif kuukausinro > 2 and kuukausinro < 6:
+    print(vuodenajat[0])
+
+elif kuukausinro > 5 and kuukausinro < 9:
+    print(vuodenajat[1])
+
+elif kuukausinro > 8 and kuukausinro < 12:
+    print(vuodenajat[2])
