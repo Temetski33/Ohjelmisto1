bde4726b236f7f1c625d2f98836bbf52d4033b4b
Temetski33 <teemu.laasio@metropolia.fi>
2024-09-10 11:08:41 +0300

    Mod7 työstöä

--- harj-07/Teht3.py
+++ harj-07/Teht3.py
@@ -1,6 +1,6 @@
 #työstö kesken
-def tehtyValinta():
-    valinta = int(input(f"Syötä uusi lentoasema kirjoita 1, hae aseman tiedot kirjoita 2, lopeta paina 3: "))
+def tehtyvalinta():
+    valinta = int(input(f"Syötä uusi lentoasema kirjoita 1, hae aseman tiedot kirjoita 2, lopeta kirjoita 3: "))
     if valinta == 0 or valinta > 4:
         print("Syötevirhe")
     elif 0 < valinta < 4:
@@ -8,13 +8,13 @@ def tehtyValinta():
 
 
 def valintafunc():
-    tehtyValinta()
+    tulos = tehtyvalinta()
 
-    if tehtyValinta == 1:
-            print("syöt")
-    elif tehtyValinta == 2:
-            print("haelol")
-    elif tehtyValinta == 3:
+    if tulos == 1:
+        print("syöt")
+    elif tulos == 2:
+        print("haelol")
+    elif tulos == 3:
         print("lopetataa")
 
 valintafunc()
