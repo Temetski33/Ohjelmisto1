bcd849dec97dded6b5bd9cbbc0f4b0e1e739ceb4
Temetski33 <teemu.laasio@metropolia.fi>
2024-09-12 09:16:50 +0300

    Moduuli 7 teht 3 KORJATTU versio

--- harj-07/Teht3.py
+++ harj-07/Teht3.py
@@ -1,15 +1,3 @@
-import mysql.connector
-
-connection = mysql.connector.connect(
-         host='127.0.0.1',
-         port= 3306,
-         database='flight_game',
-         user='teemu',
-         password='salakala',
-         autocommit=True,
-        collation='utf8mb4_general_ci'
-         )
-
 def tehtyvalinta():
     valinta = int(input(f"Syötä uusi lentoasema kirjoita 1, hae aseman tiedot kirjoita 2, lopeta kirjoita 3: "))
     if valinta == 0 or valinta >= 4:
@@ -18,32 +6,30 @@ def tehtyvalinta():
         return valinta
 
 
-def add_country(code, name):
-    sql = f"INSERT INTO country VALUES  ('{code}', '{name}', null, null, null)"
-    cursor = connection.cursor()
-    cursor.execute(sql)
-
 def valintafunc():
     tulos = tehtyvalinta()
 
     if tulos == 1:
         print(f"Syötetään uusi lentoasema.")
-        country_name = input("Anna lisättävän maan nimi: ")
-        country_code = input("Anna lisättävän maan koodi: ")
-        add_country(country_code, country_name)
+        icao = input(f"Syötä aseman ICAO-koodi: ")
+        asema = input(f"Syötä aseman nimi: ")
+        lentoasemat[icao] = asema
     elif tulos == 2:
         print(f"Haetaan aseman tiedot.")
-        cursor = connection.cursor()
-        haettukoodi = input(f"Syötä haettavan seman ICAO koodi: ")
-        sql2 = "SELECT name, iso_country FROM country"
-        cursor.execute(sql2)
-        result = cursor.fetchall()
-        haettumaa = next((i for i, elem in enumerate(result) if elem[1] == haettukoodi), -1)
-        print(f"Koodiasi vastaava lentoasema on {result[haettumaa][0]}.")
+        haku = input("Anna ICAO-koodi: ")
+        if haku in lentoasemat:
+            print(f"Koodin {haku} lentoasema on {lentoasemat[haku]}.")
+        else:
+            print(f"Haetulla koodilla ei löytynyt asemaa.")
     elif tulos == 3:
         print(f"Lopetetaan ohjelma.")
     return tulos
 
+lentoasemat = {}
+print(lentoasemat)
+
 toistokerroin = 1
 while 0 < toistokerroin < 3:
     toistokerroin = valintafunc()
+
+print(lentoasemat)
