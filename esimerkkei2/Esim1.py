3a011fbe1dbbf0ea86b2f1851450c36dbc987a0a
Temetski33 <teemu.laasio@metropolia.fi>
2024-10-21 10:00:30 +0300

    Ohjelmisto 2 alun tuntiesimerkkeja

---
+++ esimerkkei2/Esim1.py
@@ -0,0 +1,32 @@
+"""class Koira:
+    pass
+
+
+koira = Koira()
+koira.nimi = "Rekku"
+koira.syntymavuosi = 2022
+
+print(f"{koira.nimi:s} on syntynyt vuonna {koira.syntymavuosi:d}.")"""
+
+"""class Koira:
+    def __init__(self, nimi, syntymavuosi):
+        self.nimi = nimi
+        self.syntymavuosi = syntymavuosi
+
+koira = Koira("Rekku", 2022)
+
+print (f"{koira.nimi:s} on syntynyt vuonna {koira.syntymavuosi:d}." )"""
+
+# maaritellaan Kissa-luokka
+
+class Kissa:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        self.omistaja = "Tuntematon"
+
+# paaohjelma
+# luodaan uusi Kissa-tyyppinen olio
+ekaKissa = Kissa("Rudolf", 1997)
+# tulostetaan kissan arvoja
+print(f"{ekaKissa.name:s} on kisuli vuosimallia {ekaKissa.age:d}.")
