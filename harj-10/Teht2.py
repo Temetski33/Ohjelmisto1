e0c79ff07bf77948124e9e9a384e85e0f8a36dcd
Temetski33 <teemu.laasio@metropolia.fi>
2024-10-25 13:46:53 +0300

    Moduuli 10 tekoa

--- harj-10/Teht2.py
+++ harj-10/Teht2.py
@@ -1,6 +1,4 @@
-# Moduuli 10 teht 1
-
-import random
+# Moduuli 10 teht 2
 
 class Hissi:
     def __init__(self, alin_kerros, ylin_kerros):
@@ -36,12 +34,31 @@ class Hissi:
         print(f"Nykyinen kerros {self.nykyinen_kerros}")
 
 
-hissi = Hissi(1, 9)
+class Talo:
+    hissilista = []
+    def __init__(self, alin_nro, ylin_nro, hissi_lkm):
+        self.alanro = alin_nro
+        self.ylinro = ylin_nro
+        self.luo_hissit(hissi_lkm)
+
+    def luo_hissit(self, lkm):
+        for nro in range(lkm):
+            uus_hissi = Hissi(self.alanro, self.ylinro)
+            Talo.hissilista.append(uus_hissi)
+
+    def aja_hissi(self, hissin_nro, kohdekerros):
+        ajettava_hissi = self.hissilista[hissin_nro - 1]
+        ajettava_hissi.siirry_kerrokseen(kohdekerros)
+
+
+"""hissi = Hissi(1, 9)
 hissi.siirry_kerrokseen(89)
 hissi.siirry_kerrokseen(7)
 hissi.siirry_kerrokseen(6)
+hissi.siirry_kerrokseen(7)
 hissi.siirry_kerrokseen(9)
 hissi.kerros_ylos()
-hissi.kerros_alas()
-hissi.kerros_ylos()
-hissi.siirry_kerrokseen(hissi.alin)
+hissi.kerros_alas()"""
+
+talo1 = Talo(1, 7, 3)
+talo1.aja_hissi(1, 5)
